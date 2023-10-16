f54e17353a2b25a96ab57a9b2f52c37baa79068b
AmirHashemi77 <amirhashemi539@gmail.com>
2023-10-16 18:59:10 +0330

    create CardSlider Component

---
+++ src/Component/CardSlider/CardSlider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CardSlider = () => {
+    return (
+        <div className='w-full px-5 my-12'>
+            <div className='flex flex-col items-center justify-center gap-10 w-full max-w-6xl mx-auto'>
+                <div className='flex items-center justify-between w-11/12 relative bg-card-slider'>
+                    <div className='flex'></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardSlider;
