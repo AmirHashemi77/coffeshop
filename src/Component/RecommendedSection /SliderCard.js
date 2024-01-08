ee3569eee622f2a2d3336815a7e146c165e987b7
AmirHashemi77 <amirhashemi539@gmail.com>
2024-01-08 06:27:08 +0330

    slider button

--- src/Component/RecommendedSection /SliderCard.js
+++ src/Component/RecommendedSection /SliderCard.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const SliderCard = () => {
+const SliderCard = ({imgurl,title,subTitle,cardWidth}) => {
+    const cardEl=useRef();
+    useEffect(()=>{
+       cardWidth(cardEl.current.getBoundingClientRect().width)
+    },[])
+   
+
     return (
-        <div className='bg-card-slider2 px-3 relative'>
+        <div ref={cardEl} className='bg-card-slider2 px-3 relative'>
             <div className='flex items-center relative mb-20'>
-                <img src="/images/product/1b18a0fb617b2a74ca210364083c4782.png" alt="" />
+                <img src={imgurl} alt="" />
                 <div className='absolute top-5 left-5'>
                     <img src="/images/icon/ph_info.svg" alt="" />
                 </div>
@@ -13,9 +19,9 @@ const SliderCard = () => {
                 </div>
             </div>
             <div className='product-slider-card-bottom'>
-                <p className='text-white text-base font-normal'>Arabica</p>
-                <span className='rotate-90 h-[1px] w-7 bg-red-500'></span>
-                <p className='text-white text-base font-normal'>Dark Roast</p>
+                <p className='text-white text-base font-normal'>{title}</p>
+                <span className='rotate-90 h-[1px] w-7'></span>
+                <p className='text-white text-base font-normal'>{subTitle}</p>
             </div>
         </div>
     );
