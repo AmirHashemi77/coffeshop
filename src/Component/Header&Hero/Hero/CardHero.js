16be1193fa70235519f424a0ae77656fa72cbb1f
AmirHashemi77 <amirhashemi539@gmail.com>
2024-01-16 23:00:04 +0330

    fix some problem

--- src/Component/Header&Hero/Hero/CardHero.js
+++ src/Component/Header&Hero/Hero/CardHero.js
@@ -3,13 +3,13 @@ import VespaIcon from '../../../icons/VespaIcon';
 
 const CardHero = () => {
     return (
-        <div className='flex item-center py-2 rounded-t-xl h-20 bg-cart-hero absolute bottom-0 md:h-28 lg:w-fit lg:h-[25%]'>
-        <img src="/images/Cup1.png" alt="cup" className='w-1/3 h-full object-contain object-center drop-shadow-lg'/>
-        <div className='flex flex-col items-center justify-between lg:justify-center lg:gap-7 px-2'>
-            <h5 className='font-dancingScript text-xl bg-text w-full whitespace-nowrap capitalize md:text-2xl md: lg:text-3xl'>but first coffee</h5>
+        <div className='flex item-center rounded-t-3xl bg-cart-hero absolute bottom-0 md:w-[28vw] lg:rounded-t-[50px]'>
+        <img src="/images/Cup1.png" alt="cup" className='w-[18.33vw] h-[18.33vw] object-contain object-center drop-shadow-lg '/>
+        <div className='flex flex-col items-center justify-center gap-2 px-2 lg:gap-7 md:absolute md:right-0 md:top-1/2 md:-translate-y-1/2'>
+            <h5 className='font-dancingScript text-xs  bg-text whitespace-nowrap capitalize md:text-[1.93vw] md:leading-7'>but first coffee</h5>
             <button className='flex items-center justify-center btn px-2 rounded-3xl'>
-                <span className='font-leiko text-xs whitespace-nowrap bg-text px-1 capitalize lg:text-lg lg:px-5'>hurry up</span>
-                <VespaIcon width='30' height='30'/>
+                <span className='font-leiko text-2xs whitespace-nowrap bg-text px-1 capitalize lg:px-5 lg:text-xs'>hurry up</span>
+                <VespaIcon width='25' height='25'/>
             </button>
             
         </div>
