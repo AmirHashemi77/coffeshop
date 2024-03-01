134e9349b4ce971250fa44d42f5521911a4c0c29
AmirHashemi77 <amirhashemi539@gmail.com>
2024-03-01 22:12:42 +0330

    Compression hero image

--- src/Component/Header&Hero/Hero/HeroMd.js
+++ src/Component/Header&Hero/Hero/HeroMd.js
@@ -17,7 +17,7 @@ const HeroMd = () => {
                     </div>
                 </div>
               <div className='relative w-full  h-full flex-1'>
-                <img src="/images/hero-img.png" alt="hero-img" className='h-full w-full'/>
+                <img src="/images/hero-img-min.png" alt="hero-img" className='h-full w-full'/>
                 <img src="/images/Coffees.png" alt="coffees" className='absolute top-0 right-0 w-3/4 h-full drop-shadow-lg'/>
 
             </div>
