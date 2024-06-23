4f78b691b3f7edb9ff57b763066612ceeac88f9f
AmirHashemi77 <amirhashemi539@gmail.com>
2024-06-24 00:54:46 +0330

    add some product  in shop

--- src/Component/Cart/CartItem.js
+++ src/Component/Cart/CartItem.js
@@ -1,52 +1,59 @@
-import React, { useContext, useLayoutEffect, useRef, useState } from 'react';
-import NumOfProduct from '../ProductDetails/NumOfProduct';
-import { authContext } from '../../context/AuthContext';
-import { useMutation } from '@tanstack/react-query';
-import { editCartHandler } from '../../services/cartHandler';
+import React, { useContext, useLayoutEffect, useRef, useState } from "react";
+import NumOfProduct from "../ProductDetails/NumOfProduct";
+import { authContext } from "../../context/AuthContext";
+import { useMutation } from "@tanstack/react-query";
+import { editCartHandler } from "../../services/cartHandler";
 
-const CartItem = ({imgUrl ,title , price , id , number}) => {
-    const {user}=useContext(authContext)
-    const [numberOfProduct,setNumberOfProduct] = useState(number)
-    const {error , isError , mutate} = useMutation({
-        mutationKey:['cartEdit'],
-        mutationFn:editCartHandler,
-        onSuccess:()=>{
-            alert("cart updated")
-        }
-    })
-    const firstUpdate = useRef(true);
-    useLayoutEffect(() => {
-        if (firstUpdate.current) {
-            firstUpdate.current = false;
-            return;
-          }
+const CartItem = ({ imgUrl, title, price, id, number }) => {
+  const { user } = useContext(authContext);
+  const [numberOfProduct, setNumberOfProduct] = useState(number);
+  const { error, isError, mutate } = useMutation({
+    mutationKey: ["cartEdit"],
+    mutationFn: editCartHandler,
+    onSuccess: () => {
+      alert("cart updated");
+    },
+  });
+  const firstUpdate = useRef(true);
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
 
-          const newCart=[...user.cart,{
-            id:id,
-            title:title,
-            price:price,
-            image:imgUrl,
-            number:numberOfProduct
-        }]
+    const newCart = [
+      ...user.cart,
+      {
+        id: id,
+        title: title,
+        price: price,
+        image: imgUrl,
+        number: numberOfProduct,
+      },
+    ];
 
-        mutate({newCartData:newCart , userId:user.id})
+    mutate({ newCartData: newCart, userId: user.id });
+  }, [numberOfProduct]);
 
-          
-    },[numberOfProduct])
-    
-    return (
-        <div className='flex flex-col gap-7 items-center justify-center w-full py-4 px-3 border-b border-white border-opacity-20 md2:flex-row md2:justify-between'>
-            {/* product img  */}
-            <div className='flex items-center justify-center'>
-                <img src={imgUrl} alt={title} className=' w-20 h-20 object-contain object-center'/>
-                <span className='text-white text-xl font-leiko leading-8 text-center lg:text-2xl capitalize opacity-90'>{title}</span>
-            </div>
-            {/* num of product  */}
-            <NumOfProduct number={numberOfProduct} setNumber={setNumberOfProduct}/>
-            {/* price  */}
-            <p className='text-white text-xl font-leiko leading-8 text-center lg:text-2xl capitalize opacity-90'>{`${price}$`}</p>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-7 items-center justify-center w-full py-4 px-3 border-b border-white border-opacity-20 md2:flex-row md2:justify-between">
+      {/* product img  */}
+      <div className="flex items-center justify-center">
+        <img
+          src={imgUrl}
+          alt={title}
+          className=" w-20 h-20 object-contain object-center"
+        />
+        <span className="text-white text-xl font-leiko leading-8 text-center lg:text-2xl capitalize opacity-90">
+          {title}
+        </span>
+      </div>
+      {/* num of product  */}
+      <NumOfProduct number={numberOfProduct} setNumber={setNumberOfProduct} />
+      {/* price  */}
+      <p className="text-white text-xl font-leiko leading-8 text-center lg:text-2xl capitalize opacity-90">{`${price}$`}</p>
+    </div>
+  );
 };
 
 export default CartItem;
